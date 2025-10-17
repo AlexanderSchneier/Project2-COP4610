5cc44d33d37dc1c957ce4af18cd3e8a755ca1e65
lps15 <lpschneier15@gmail.com>
2025-10-17 14:03:22 -0400

    part1

--- part1/part1.c
+++ part1/part1.c
@@ -1,13 +1,33 @@
 #include <unistd.h> 
 
 
+#include <stdio.h>
+#include <unistd.h>   // all syscalls used here
 
-int main(){
+int main() {
+    // 1. getpid()
+    pid_t pid = getpid();
+    printf("Process ID: %d\n", pid);
 
-    getpid();
-    write();
-    open();
-    read();
-    time();
-    return -1;
+    // 2. getppid()
+    pid_t ppid = getppid();
+    printf("Parent Process ID: %d\n", ppid);
+
+    // 3. write() - to stdout (fd = 1)
+    const char *msg = "Hello from write()\n";
+    write(1, msg, 18);
+
+    // 4. read() - read user input from stdin (fd = 0)
+    char buffer[50];
+    ssize_t bytes = read(0, buffer, sizeof(buffer)-1);
+    if (bytes > 0) {
+        buffer[bytes] = '\0';
+        printf("You typed: %s", buffer);
+    }
+
+    // 5. close() - here we’ll close stderr (fd = 2) just for demo
+    close(2);  
+    // After this, errors won’t print because stderr is closed
+
+    return 0;
 }
