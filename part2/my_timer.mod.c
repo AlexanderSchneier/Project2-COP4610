651f7a895e1f02f76cf951300d911343d66336c1
cbacary <cbacary4@gmail.com>
2025-11-03 14:37:48 -0500

    Update gitignore

--- part2/my_timer.mod.c
+++ part2/my_timer.mod.c
@@ -18,46 +18,52 @@ __section(".gnu.linkonce.this_module") = {
 
 static const struct modversion_info ____versions[]
 __used __section("__versions") = {
-	{ 0xb9e81daf, "proc_remove" },
+	{ 0x0380b27c, "proc_remove" },
+	{ 0xbd03ed67, "__ref_stack_chk_guard" },
 	{ 0xf46d5bf3, "mutex_lock" },
 	{ 0x680628e7, "ktime_get_real_ts64" },
 	{ 0x40a621c5, "scnprintf" },
 	{ 0xf46d5bf3, "mutex_unlock" },
+	{ 0x546c19d9, "validate_usercopy_range" },
 	{ 0xa61fd7aa, "__check_object_size" },
 	{ 0x092a35a2, "_copy_to_user" },
 	{ 0xf64ac983, "__copy_overflow" },
 	{ 0xd272d446, "__stack_chk_fail" },
 	{ 0xe4de56b4, "__ubsan_handle_load_invalid_value" },
 	{ 0xd272d446, "__fentry__" },
-	{ 0xf8d7ac5e, "proc_create" },
+	{ 0x8c95c720, "proc_create" },
 	{ 0xd272d446, "__x86_return_thunk" },
-	{ 0x70eca2ca, "module_layout" },
+	{ 0x7b06df01, "module_layout" },
 };
 
 static const u32 ____version_ext_crcs[]
 __used __section("__version_ext_crcs") = {
-	0xb9e81daf,
+	0x0380b27c,
+	0xbd03ed67,
 	0xf46d5bf3,
 	0x680628e7,
 	0x40a621c5,
 	0xf46d5bf3,
+	0x546c19d9,
 	0xa61fd7aa,
 	0x092a35a2,
 	0xf64ac983,
 	0xd272d446,
 	0xe4de56b4,
 	0xd272d446,
-	0xf8d7ac5e,
+	0x8c95c720,
 	0xd272d446,
-	0x70eca2ca,
+	0x7b06df01,
 };
 static const char ____version_ext_names[]
 __used __section("__version_ext_names") =
 	"proc_remove\0"
+	"__ref_stack_chk_guard\0"
 	"mutex_lock\0"
 	"ktime_get_real_ts64\0"
 	"scnprintf\0"
 	"mutex_unlock\0"
+	"validate_usercopy_range\0"
 	"__check_object_size\0"
 	"_copy_to_user\0"
 	"__copy_overflow\0"
--- part3/tests/system-calls-test/.syscheck.mod.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_syscheck.mod := printf '%s\n'   syscheck.o | awk '!x[$$0]++ { print("./"$$0) }' > syscheck.mod
